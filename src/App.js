42f4fc6c74ba67e7e43b69b1f2753f2f70bc1438
woolfenr <rosswoolfenden@gmail.com>
2020-10-28 14:07:02 +0000

    added colours and shti

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import off from './img/red.jpg';
-import on from './img/green.jpg'
+import on from './img/green.jpg';
+import cry from './img/joshcry.jpg';
+import kill from './img/joshkillme.jpg';
 import './App.css';
 import axios from 'axios';
 
@@ -14,34 +16,56 @@ function App() {
   // let light = true;
   // let stringlight = 'POWER IS ON'
   const [light, setLight] = useState(1);
-  const [stringLight, setStringLight] = useState("On")
+  const [stringLight, setStringLight] = useState("POWER IS ON");
+  const [curimg, setCurimg] = useState(0);
+  const [ccs, setCcs] = useState('App-header')
+  let img = kill;
   // console.log(light);
 
+  const imgages = [kill, cry];
+  // useEffect(() => {
+  //   if(light) {
+  //     img = cry;
+  //   } else {
+  //     img = kill;
+  //   }
+  // }) ;
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={ccs} >
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
         <button onClick={() => fuckWithJosh(light) }>
-          <img src={off} className="onoff" />
+          <img src={ imgages[curimg] } className="onoff" />
         </button>
         <p>
-            LETS FUCK WITH HARVEY
+            TOUCH HIM TO MAKE HIM CRY
         </p>
         <h1> { stringLight }</h1>
       </header>
     </div>
   );
-
+  async function whatimg() {
+    if(light) {
+      return kill;
+    } else {
+      return cry;
+    }
+  }
   async function fuckWithJosh() {
     if(light == true) {
       await lightToggle(false);
       setLight(false);
       setStringLight('POWER IS OFF')
+      setCurimg(1);
+      setCcs("App-header2")
       
     } else {
       await lightToggle(true);
       setLight(true);
       setStringLight('POWER IS ON');
+      setCurimg(0);
+      setCcs("App-header")
     }
   }
 
